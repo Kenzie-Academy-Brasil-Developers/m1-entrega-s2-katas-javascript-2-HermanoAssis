988064cb98e25b81d5be3561a400210c25ab4a8f
hermano <hermano_assis@hotmail.com>
2022-06-28 16:13:48 -0300

    Katas2

--- katas2.js
+++ katas2.js
@@ -1,29 +1,48 @@
 // comece a criar a sua função add na linha abaixo
-
+function add(a,b){
+    return a+b
+}
 
 // descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
+console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
 
 
 // comece a criar a sua função multiply na linha abaixo
-
+function multiply(a,b){
+    let result = 0;
+    for(let i = 0; 0<b; i++){
+        result += add(a, result)
+    }
+    return result
+}
 
 // descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
 
 
 // comece a criar a sua função power na linha abaixo
-
-
+function power(x,n){
+    let expo = x;
+    for(let i = 1; i != n; i++){
+        expo = multiply(expo, x);
+    }
+    return expo
+}
 // descomente a linha seguinte para testar sua função
-// console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
+console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
 
 
 // comece a criar a sua função factorial na linha abaixo
-
+    function factorial(a){
+        let result = 1;
+        for(let i = 1; i > 0; i--){
+            result = multiply(i, result);
+        }
+        return result
+    }
 
 // descomente a linha seguinte para testar sua função
-// console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
+console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
 
 
 /**
@@ -31,7 +50,15 @@
  */
 
 // crie a função fibonacci
-
+function fibonacci(num){
+    let fibona = [0]
+    let adi = 1
+    for(let i = 0; i <= num; i = add(i, 1)){
+        fibona.push(add(fibona[i], adi))
+        adi = fibona[i]
+    }
+    return fibona[i]
+}
 
 // descomente a linha seguinte para testar sua função
 // console.assert(fibonacci(7) === 13, 'A função fibonacci não está funcionando como esperado');
